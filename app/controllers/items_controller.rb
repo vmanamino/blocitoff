f0df0c486db91d8aab03d05b83c412fd57864964
Viral Amin <vmanamino@gmail.com>
2015-06-09 04:53:48 +0000

    created Item and relevant views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,20 +3,17 @@ class ItemsController < ApplicationController
     @user = User.find(params[:user_id])
     @item = Item.new
   end
-  
+
   def create
     @user = User.find(params[:user_id])
     @item = current_user.items.build(params.require(:item).permit(:name))
     @item.user = @user
-    
     if @item.save
-      flash[:notice] = "Item was saved"
+      flash[:notice] = 'Item was saved'
       redirect_to [@user]
     else
-      flash[:error] = "There was an error, your item was not saved!"
+      flash[:error] = 'There was an error, your item was not saved!'
       render :new
     end
-    
   end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     authorize @user
-    @name = @user.name
-    @email = @user.email
-    @items = @user.items    
+    @items = @user.items
   end
 end
